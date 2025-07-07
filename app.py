fd18f1f2a43e7dc12e23a5e53c70c1a67a6a6e80
anoohya10 <anoohyamateti10@gmail.com>
2025-07-07 22:11:00 +0530

    files added

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Enable CORS for frontend requests
+
+@app.route('/verify',methods=['GET'])
+def verify():
+    return jsonify({
+        'status': 'verified',
+        'redirect': '/VerifiedPage'
+    })
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
